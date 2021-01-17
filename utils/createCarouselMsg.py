c4f2e4ea84c5723c9fc0b090277b04f48d17c03d
JiaAnTW <at5lp6andy@gmail.com>
2021-01-17 15:30:16 +0800

    feat: init createCarouselMsg

---
+++ utils/createCarouselMsg.py
@@ -0,0 +1,31 @@
+from linebot.models import MessageTemplateAction, URITemplateAction, PostbackTemplateAction, TemplateSendMessage, CarouselTemplate, CarouselColumn
+
+
+def createCarouselMsg(msgArr):
+    outputMsg = []
+    for msg in msgArr:
+        outputMsg.append(
+            CarouselColumn(
+                thumbnail_image_url=msg['imageUrl'],
+                title=msg['title'],
+                text=msg['description'],
+                actions=[
+                    MessageTemplateAction(
+                        label='說明/description',
+                        text=msg['title']+'的說明'
+                    ),
+                    URITemplateAction(
+                        label='連結',
+                        uri=msg['link']
+                    ),
+                    MessageTemplateAction(
+                        label='返回/back',
+                        text='返回/back'
+                    ),
+                ]
+            )
+        )
+    return TemplateSendMessage(
+        alt_text='Carousel template',
+        template=CarouselTemplate(columns=outputMsg)
+    )
